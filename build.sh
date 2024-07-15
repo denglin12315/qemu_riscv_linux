8b419c61cd344ddf0bbe67497349bdbc3abfe6cd
ldeng <denglin12315@163.com>
2024-07-15 21:39:18 +1400

    add qemu build.sh
    Signed-off-by: ldeng <denglin12315@163.com>

---
+++ build.sh
@@ -0,0 +1,8 @@
+SHELL_FOLDER=$(pwd)
+
+if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then  
+./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
+fi  
+make -j
+#make install
+#cd ..
