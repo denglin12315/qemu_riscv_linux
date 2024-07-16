1b8c8f142b42834c13511576ebb376aa5d1c28d1
ldeng <denglin12315@163.com>
2024-07-17 00:02:14 +1400

    添加低阶启动代码工程，暂时用于测试串口和pflash
    Signed-off-by: ldeng <denglin12315@163.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 SHELL_FOLDER=$(pwd)
 
-if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then  
-./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
-fi  
-make -j
-#make install
-#cd ..
+case $1 in
+"qemu")
+	#####################qemu compile
+	./configure --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
+	make -j
+;;
+"start")
+	####################lowlevel start code compile
+	CROSS_PREFIX=riscv64-linux
+
+	cd $SHELL_FOLDER/lowlevelboot
+	$CROSS_PREFIX-gcc -x assembler-with-cpp -c startup.s -o $SHELL_FOLDER/lowlevelboot/startup.o
+	$CROSS_PREFIX-gcc -nostartfiles -T./boot.lds -Wl,-Map=$SHELL_FOLDER/lowlevelboot/lowlevel_fw.map -Wl,--gc-sections $SHELL_FOLDER/lowlevelboot/startup.o -o $SHELL_FOLDER/lowlevelboot/lowlevel_fw.elf
+	$CROSS_PREFIX-objcopy -O binary -S $SHELL_FOLDER/lowlevelboot/lowlevel_fw.elf $SHELL_FOLDER/lowlevelboot/lowlevel_fw.bin
+	$CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/lowlevelboot/lowlevel_fw.elf > $SHELL_FOLDER/lowlevelboot/lowlevel_fw.lst
+
+	#create new fw.bin
+	rm -rf fw.bin
+	dd of=fw.bin bs=1k count=32k if=/dev/zero
+	dd of=fw.bin bs=1k conv=notrunc seek=0 if=lowlevel_fw.bin
+	cd $SHELL_FOLDER
+;;
+esac
--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ $SHELL_FOLDER/build/qemu-system-riscv64 \
 -M quard-star \
 -m 1G \
 -smp 8 \
+-drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/lowlevelboot/fw.bin \
 -nographic --parallel none
 
