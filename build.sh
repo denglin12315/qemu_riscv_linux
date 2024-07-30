0d29a883621b71b9579b76e12d3a8014ac7de0d6
lin.deng <denglin12315@163.com>
2024-07-30 14:18:02 +0800

    制作硬盘镜像，包含两个分区: 1.boot分区，存放内核Image文件,设备树文件以及uboot的boot.scr 2.主文件系统分区
    Signed-off-by: lin.deng <denglin12315@163.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,17 @@ SHELL_FOLDER=$(pwd)
 CROSS_PREFIX=/opt/riscv64--glibc--bleeding-edge-2020.08-1/bin/riscv64-linux
 
 case $1 in
+"linux")
+    cd $SHELL_FOLDER/linux-5.10.42
+    make ARCH=riscv CROSS_COMPILE=$CROSS_PREFIX- defconfig
+    make ARCH=riscv CROSS_COMPILE=$CROSS_PREFIX- -j1
+    cd -
+;;
+"linux_clean")
+    cd $SHELL_FOLDER/linux-5.10.42
+    make ARCH=riscv CROSS_COMPILE=$CROSS_PREFIX- distclean
+    cd -
+;;
 "qemu")
 	#####################qemu compile
     cd $SHELL_FOLDER/qemu-6.0.0
@@ -10,10 +21,21 @@ case $1 in
 	bear make -j
     cd -
 ;;
+"qemu_clean")
+    cd $SHELL_FOLDER/qemu-6.0.0
+	./configure --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio --enable-debug
+    make clean
+    cd -
+;;
 "dts")
     cd $SHELL_FOLDER/dts
     dtc -I dts -O dtb -o ./quard_star_sbi.dtb ./quard_star_sbi.dts
     dtc -I dts -O dtb -o ./quard_star_uboot.dtb ./quard_star_uboot.dts
+    cd -
+;;
+"dts_clean")
+    cd $SHELL_FOLDER/dts
+    rm -rf *.dtb
     cd -
 ;;
 "opensbi")
@@ -23,12 +45,27 @@ case $1 in
     $CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/opensbi-0.9/fw_jump.elf > $SHELL_FOLDER/opensbi-0.9/fw_jump.lst
     cd -
 ;;
+"opensbi_clean")
+    cd $SHELL_FOLDER/opensbi-0.9
+    make CROSS_COMPILE=$CROSS_PREFIX- PLATFORM=quard_star clean 
+    rm -rf opensbi-0.9/fw_jump.lst
+    cd -
+;;
 "trusted_fw")
     cd $SHELL_FOLDER/trusted_fw
     $CROSS_PREFIX-gcc -x assembler-with-cpp -c startup.s -o $SHELL_FOLDER/trusted_fw/startup.o
     $CROSS_PREFIX-gcc -nostartfiles -T./link.lds -Wl,-Map=$SHELL_FOLDER/trusted_fw/trusted_fw.map -Wl,--gc-sections $SHELL_FOLDER/trusted_fw/startup.o -o $SHELL_FOLDER/trusted_fw/trusted_fw.elf
     $CROSS_PREFIX-objcopy -O binary -S $SHELL_FOLDER/trusted_fw/trusted_fw.elf $SHELL_FOLDER/trusted_fw/trusted_fw.bin
     $CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/trusted_fw/trusted_fw.elf > $SHELL_FOLDER/trusted_fw/trusted_fw.lst
+    cd -
+;;
+"trusted_fw_clean")
+    cd $SHELL_FOLDER/trusted_fw
+    rm -rf *.o
+    rm -rf *.bin
+    rm -rf *.elf
+    rm -rf *.lst
+    cd -
 ;;
 "bl0")
 	####################lowlevel start code compile
@@ -47,12 +84,27 @@ case $1 in
     dd of=fw.bin bs=1k conv=notrunc seek=2K if=$SHELL_FOLDER/opensbi-0.9/build/platform/quard_star/firmware/fw_jump.bin
     dd of=fw.bin bs=1k conv=notrunc seek=4K if=$SHELL_FOLDER/trusted_fw/trusted_fw.bin
     dd of=fw.bin bs=1k conv=notrunc seek=8K if=$SHELL_FOLDER/u-boot-2021.07/u-boot.bin
-
-    #create fs
-    cd $SHELL_FOLDER/rootfs
-    rm -rf rootfs.img
-    dd of=rootfs.img bs=1k count=32k if=/dev/zero
-	cd $SHELL_FOLDER
+;;
+"bl0")
+    cd $SHELL_FOLDER/bl0
+    rm -rf *.o
+    rm -rf *.bin
+    rm -rf *.elf
+    rm -rf *.lst
+    cd -
+;;
+"fs")
+    cd $SHELL_FOLDER/fs
+    rm -rf ./fs/rootfs/rootfs.img
+    dd if=/dev/zero of=./rootfs/rootfs.img bs=1M count=1024
+    sleep 1
+    pkexec $SHELL_FOLDER/fs/generate_rootfs.sh $SHELL_FOLDER/fs/rootfs/rootfs.img $SHELL_FOLDER/fs/sfdisk
+    sleep 1
+    cp $SHELL_FOLDER/linux-5.10.42/arch/riscv/boot/Image $SHELL_FOLDER/fs/bootfs/Image
+    cp $SHELL_FOLDER/dts/quard_star_uboot.dtb $SHELL_FOLDER/fs/bootfs/quard_star.dtb
+    $SHELL_FOLDER/u-boot-2021.07/tools/mkimage -A riscv -O linux -T script -C none -a 0 -e 0 -n "Distro Boot Script" -d $SHELL_FOLDER/dts/quard_star_uboot.cmd $SHELL_FOLDER/fs/bootfs/boot.scr
+    pkexec $SHELL_FOLDER/fs/build.sh $SHELL_FOLDER/fs
+    cd -
 ;;
 "uboot")
     cd $SHELL_FOLDER/u-boot-2021.07
@@ -61,22 +113,30 @@ case $1 in
     $CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/u-boot-2021.07/u-boot > $SHELL_FOLDER/u-boot-2021.07/u-boot.lst
     cd -
 ;;
+"uboot_clean")
+    cd $SHELL_FOLDER/u-boot-2021.07
+    make CROSS_COMPILE=$CROSS_PREFIX- clean
+    rm -rf u-boot.lst
+    cd -
+;;
 "clean")
-    #cd $SHELL_FOLDER/qemu-6.0.0
-    #./configure --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio --enable-debug
-    #make clean
-    #cd -
+    cd $SHELL_FOLDER/qemu-6.0.0
+    ./configure --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio --enable-debug
+    make clean
+    cd -
 
     cd $SHELL_FOLDER/dts
-    rm -rf ./quard_star_sbi.dtb
+    rm -rf ./*.dtb
     cd -
 
     cd $SHELL_FOLDER/opensbi-0.9
     make CROSS_COMPILE=$CROSS_PREFIX- PLATFORM=quard_star clean
+    rm -rf fw_jump.lst
     cd -
 
     cd $SHELL_FOLDER/u-boot-2021.07
     make CROSS_COMPILE=$CROSS_PREFIX- distclean
+    rm -rf u-boot.lst
     cd -
 
     cd $SHELL_FOLDER/bl0
@@ -95,7 +155,7 @@ case $1 in
 
     cd $SHELL_FOLDER/rootfs
     rm -rf rootfs.img
-    cd $SHELL_FOLDER
+    cd -
 
     rm -rf fw.bin
 ;;
--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ $SHELL_FOLDER/qemu-6.0.0/build/qemu-system-riscv64 \
 -m 1G \
 -smp 8 \
 -drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/bl0/fw.bin \
--drive file=$SHELL_FOLDER/rootfs/rootfs.img,format=raw,id=hd0 \
+-drive file=$SHELL_FOLDER/fs/rootfs/rootfs.img,format=raw,id=hd0 \
 -device virtio-blk-device,drive=hd0 \
 -fw_cfg name="opt/qemu_cmdline",string="qemu_vc="$DEFAULT_V"" \
 --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --monitor vc:$DEFAULT_VC --parallel none
