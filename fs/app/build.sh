d369a3241393a227d0c9ea6361e1fb609572985e
lin.deng <denglin12315@163.com>
2024-08-21 16:13:26 +0800

    添加zlib-1.3.1 + openssl-3.0.0 + openssh-8.6p1代码
    Signed-off-by: lin.deng <denglin12315@163.com>

--- fs/app/build.sh
+++ fs/app/build.sh
@@ -57,6 +57,27 @@ build)
 	make -j16
 	make install-binaries
 	cd -
+	# 编译zlib
+	cd $SHELL_FOLDER/zlib-1.3.1
+	export CC=$CROSS_PREFIX-gcc
+	./configure --prefix=$2
+	make -j2
+	make install
+	cd -
+	# 编译openssl
+	cd $SHELL_FOLDER/openssl-3.0.0
+	export CC=$CROSS_PREFIX-gcc
+	./configure linux-generic64 no-asm --prefix=$2 --cross-compile-prefix=$CROSS_PREFIX-
+	make -j2
+	make install_sw
+	cd -
+
+	# 编译openssh
+	cd $SHELL_FOLDER/ssh
+	./configure --host=riscv64-linux-gnu --with-openssl=$2 --with-zlib=$2 CXX=$CROSS_PREFIX-g++ CC=$CROSS_PREFIX-gcc
+	make -j2
+	make install
+	cd -
 
 	# cp qt lib to rootfs /opt dir
 	mkdir -p $2/opt/qt-5.15.10
